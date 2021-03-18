7c629af3a75774a48074c5603c84f2731a1a597e
lucasparente1998 <72233781+lucasparente1998@users.noreply.github.com>
2021-03-18 18:14:48 -0300

    Projeto aluraFlix

---
+++ src/components/Menu/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+import Logo from '../../assets/img/Logo.png'
+import './Menu.css'
+// import ButtonLink from './components/ButtonLink';
+import Button from "../Button/index";
+
+function Menu(){
+    return(
+        <nav className="Menu">
+            <Link to="/">
+                <img className="Logo" src={Logo} alt="AluraFlix logo"></img>
+            </Link>
+
+            <Button as={Link} className="ButtonLink" to="/cadastro/video">
+                Novo vídeo
+            </Button>
+        </nav>
+    )
+}
+
+export default Menu;
